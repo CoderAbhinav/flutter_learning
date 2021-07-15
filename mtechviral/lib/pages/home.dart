541fe50cdc7044fcde05d14358232874a67c8ca8
CoderAbhinav <abhinavrb.a02@gmail.com>
2021-07-15 15:36:14 +0530

    Done for today

--- mtechviral/lib/pages/home.dart
+++ mtechviral/lib/pages/home.dart
@@ -1,14 +1,39 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mtechviral/widgets/drawer.dart';
 import 'package:mtechviral/models/catalogue.dart';
 import 'package:mtechviral/widgets/iteam_widget.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 3));
+    final catalogJSON =
+        await rootBundle.loadString("android/assets/files/catalog.json");
+    var catalogDecode = jsonDecode(catalogJSON);
+    var prdcat = catalogDecode["products"];
+    CatalogModel.items =
+        List.from(prdcat).map<Item>((e) => Item.fromMap(e)).toList();
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dumyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.deepPurple,
@@ -17,15 +42,18 @@ class Home extends StatelessWidget {
       ),
       drawer: MyDrawer(),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dumyList.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(
-                item: dumyList[index],
-              );
-            }),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? ListView.builder(
+                  itemCount: CatalogModel.items.length,
+                  itemBuilder: (context, index) {
+                    return ItemWidget(
+                      item: CatalogModel.items[index],
+                    );
+                  })
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
     );
   }
 }
